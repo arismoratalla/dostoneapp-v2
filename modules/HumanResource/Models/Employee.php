2915550832da2d30aceae3dee53cfa1e2d014e82
Aris Moratalla <arismoratalla@gmail.com>
2024-02-18 22:54:20 +0800

    added modules directory and created HumanResource module

---
+++ modules/HumanResource/Models/Employee.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\HumanResource\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function division()
+    {
+        return $this->belongsTo(Division::class);
+    }
+}
---
+++ modules/HumanResource/routes.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('hr')->group(function () {
+    Route::get('/', [Modules\HumanResource\Http\Controllers\HumanResourceController::class, 'index'])->name('hr.index');
+
+    Route::prefix('leave')->group(function () {
+        Route::get('/', [Modules\HumanResource\Http\Controllers\LeaveController::class, 'index'])->name('leave.index');
+        Route::post('/store', [Modules\HumanResource\Http\Controllers\LeaveController::class, 'store'])->name('leave.store');
+    });
+
+    Route::prefix('leavetypes')->group(function () {
+        Route::get('/', [Modules\HumanResource\Http\Controllers\LeaveTypeController::class, 'index'])->name('leavetype.index');
+    });
+
+    // Route::prefix('employee')->group(function () {
+    //     Route::get('/', [Modules\HumanResource\Http\Controllers\LeaveController::class, 'index'])->name('leave.index');
+    //     Route::post('/store', [Modules\HumanResource\Http\Controllers\LeaveController::class, 'store'])->name('leave.store'); 
+    // });
+
+    Route::prefix('officer')->group(function () {
+        // Route::get('/', [Modules\HumanResource\Http\Controllers\LeaveController::class, 'index'])->name('leave.index');
+        // Route::post('/store', [Modules\HumanResource\Http\Controllers\LeaveController::class, 'store'])->name('leave.store'); 
+    });
+
+    Route::prefix('recommender')->group(function () {
+        // Route::get('/', [Modules\HumanResource\Http\Controllers\LeaveController::class, 'index'])->name('leave.index');
+        // Route::post('/store', [Modules\HumanResource\Http\Controllers\LeaveController::class, 'store'])->name('leave.store'); 
+    });
+
+    Route::prefix('approver')->group(function () {
+        // Route::get('/', [Modules\HumanResource\Http\Controllers\LeaveController::class, 'index'])->name('leave.index');
+        // Route::post('/store', [Modules\HumanResource\Http\Controllers\LeaveController::class, 'store'])->name('leave.store'); 
+    });
+});
